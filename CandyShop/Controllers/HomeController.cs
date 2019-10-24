98a6df295297d15c32c543459a44a809e4a0faa2
Pavol <pavol@homerundesign.com>
2019-10-23 20:38:14 -0400

    Adding Home Controller and View

---
+++ CandyShop/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CandyShop.Models;
+using CandyShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CandyShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ICandyRepository _candyRepository;
+
+        public HomeController(ICandyRepository candyRepository)
+        {
+            _candyRepository = candyRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var homeViewModel = new HomeViewModel
+            {
+                CandyOnSale = _candyRepository.GetCandyOnSale
+            };
+
+            return View(homeViewModel);
+        }
+    }
+}
---
+++ CandyShop/ViewModels/HomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using CandyShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CandyShop.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Candy> CandyOnSale { get; set; }
+    }
+}
